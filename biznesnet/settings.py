be17e462cd637fdf14a3df92e0bb70d44c79fbf1
Murodjon Xatamov <xatamovm992@gmail.com>
2025-08-20 04:39:30 -0600

    Configure STATIC_ROOT and Procfile for Render deploy

--- biznesnet/settings.py
+++ biznesnet/settings.py
@@ -137,3 +137,10 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+import os
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
